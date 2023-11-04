903de6b3c8c644263d1059d9412c3c78619e8b7a
Arnav Prabhu <arnavprabhu27@gmail.com>
2023-11-04 17:41:01 -0500

    Made Major revamp to insurance calculation

--- SmallBusinessInsurance.py
+++ SmallBusinessInsurance.py
@@ -1,11 +1,26 @@
-import random
-
 # Define an AI-driven risk assessment module
 class RiskAssessment:
     @staticmethod
-    def assess_risk(business_name):
-        # Simulate a basic risk assessment using random values
-        risk_score = random.randint(1, 100)
+    def assess_risk(business_name, industry, location):
+        # Simulate a basic risk assessment based on user inputs
+        risk_score = 0
+        
+        # Assess risk based on industry
+        if industry.lower() == "retail":
+            risk_score += 10
+        elif industry.lower() == "manufacturing":
+            risk_score += 20
+        elif industry.lower() == "technology":
+            risk_score += 5
+
+        # Assess risk based on location
+        if location.lower() == "urban":
+            risk_score += 15
+        elif location.lower() == "suburban":
+            risk_score += 10
+        elif location.lower() == "rural":
+            risk_score += 5
+
         return risk_score
 
 # Define an AI-powered chatbot for customer support
@@ -20,13 +35,21 @@ class Chatbot:
         else:
             return "I'm sorry, I cannot answer that question right now."
 
+# Get user inputs for business attributes
+business_name = input("Enter your business name: ")
+industry = input("Enter your industry (e.g., retail, manufacturing, technology): ")
+location = input("Enter your business location (e.g., urban, suburban, rural): ")
+
+# Calculate the risk factor based on user inputs
+risk_score = RiskAssessment.assess_risk(business_name, industry, location)
+
 # Update the SmallBusinessInsurance class to integrate AI and risk-based premiums
 class SmallBusinessInsurance:
-    def __init__(self, business_name, coverage_type):
+    def __init__(self, business_name, coverage_type, risk_score):
         self.business_name = business_name
         self.coverage_type = coverage_type
         self.claims = []
-        self.risk_score = RiskAssessment.assess_risk(business_name)
+        self.risk_score = risk_score
         self.premium = self.calculate_premium()
 
     def calculate_premium(self):
@@ -53,20 +76,20 @@ class SmallBusinessInsurance:
 
 # Example usage
 if __name__ == "__main__":
-    # Create a small business insurance policy
-    policy1 = SmallBusinessInsurance("ABC Inc.", "General Liability")
+    # Create a small business insurance policy with user-input risk factor
+    policy1 = SmallBusinessInsurance(business_name, "General Liability", risk_score)
     
-    # Display policy information, including risk assessment and premium calculation
+    # Display policy information, including user-defined risk assessment
     policy1.display_policy_info()
     
     # File a claim
-    policy1.file_claim("Property damage due to a fire")
-    policy1.file_claim("Customer slip and fall accident")
+    claim_description = input("Enter the claim description: ")
+    policy1.file_claim(claim_description)
 
     # Display updated policy information with claims and adjusted premium
     policy1.display_policy_info()
 
     # Interact with the AI-powered chatbot
-    user_question = "What is my premium?"
+    user_question = input("Ask a question (e.g., 'What is my premium?'): ")
     chatbot_response = Chatbot.answer_question(user_question)
     print(f"Chatbot Response: {chatbot_response}")
